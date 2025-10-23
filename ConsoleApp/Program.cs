a0da39fb9b87f9be4445e060ead8cdfb1b3f1931
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2025-10-23 11:17:19 +0300

    отладочка

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            constProp cs = constProp.Title;
+
+            var ss=cs.ToString();
+
+            Dictionary<string, string> customProperties = new Dictionary<string, string>()
+         {
+             {"prop10<>/\":;?*|,='Ok<>/\":;?*|,='Ok", "val1"},
+             {" ", "val2"},
+             {"", "val2"},
+                  {"prop3", "val3"},
+             {"prop4", "val3"},
+             {"prop40", ""},
+         };
+            bool bb = customProperties.ContainsKey("pro p4");
+
+            var rr = customProperties["prop45"];
+
+            foreach (var item in customProperties)
+            {
+
+
+                string key = item.Key;
+                if (string.IsNullOrWhiteSpace(key))
+                {
+                    key = "_";
+                }
+
+                string pattern = @"[\<\>\/\:\\\"";\?\*\|\,\=\']";
+                key = Regex.Replace(key, pattern, "_");
+                key = Regex.Replace(key, @"_+", "_");
+
+                Console.WriteLine($"{item.Key}:\t{key} ->{item.Value}");
+            }
+
+
+
+            //string key = "test<>/\":;?*|,='Ok";
+            //string pattern = @"[\<\>\/\:\\\"";\?\*\|\,\=\']";
+            //key = Regex.Replace(key, pattern, "_");
+
+            //string secondPattern = @"_+"; // Одно или более подчеркиваний
+
+            //// Окончательная строка после второй замены
+            //string finalResult = Regex.Replace(key, secondPattern, "_");
+
+            //key = Regex.Replace(key, @"_+", "_");
+            //Console.WriteLine(finalResult);
+        }
+    }
+
+    internal enum constProp
+    {
+        None,
+        Title,
+        Subject,
+        RevisionNumber,
+        LastSavedBy,
+        Keywords,
+        HyperlinkBase,
+        Comments,
+        Autor,
+    }
+}
