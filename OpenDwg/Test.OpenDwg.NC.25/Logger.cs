9916af779457a4024b851b452852aeddb81d0f2e
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2025-12-26 11:51:15 +0300

    добавил в лог вызывающего , очистку по дате, перегрузки

--- OpenDwg/Test.OpenDwg.NC.25/Logger.cs
+++ OpenDwg/Test.OpenDwg.NC.25/Logger.cs
@@ -3,56 +3,44 @@ using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Text;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace dRz.Test.OpenDwg
 {
     public class Logger
     {
+        public Logger(int dateSpan, string appName = @"logger")
+        {
+
+            Init(appName);
+
+            _dateSpan = dateSpan;
+
+            LogDelToDate();
+
+        }
+
         public Logger(string appName = @"logger")
         {
-            string date = DateTime.Now.ToString("yyyyMMdd-HH_mm_ss",
-                                CultureInfo.InvariantCulture);
+            Init(appName);
+        }
+
+
+        void Init(string appName)
+        {
+            string date = DateTime.Now.ToString("yyyyMMdd-HH_mm_ss", CultureInfo.InvariantCulture);
 
             _appName = $"{date}_{appName}.log";
 
         }
 
-        public  void LogComplete(string message)
-        {
-            string date = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss.FFFFF",
-                                CultureInfo.InvariantCulture);
-
-            string str;
-            using (StreamReader sreader = new StreamReader(_path))
-            {
-                str = sreader.ReadToEnd();
-            }
-
-
-            string text = $"{date}: {message}";
-
-            using (StreamWriter writer = new StreamWriter(_path, false, Encoding.UTF8))
-            {
-                  writer.WriteLine(text);
-                  writer.WriteLine(str);
-
-            }
-        }
-
-        public void LogClear()
-        {
-            if (File.Exists(_path))
-            {
-                File.Delete(_path);
-            }
-        }
-        public async void Log(string message)
+        public async void Log(string message, [CallerMemberName] string caller = null)
         {
 
-            string date = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss.FFFFF",
-                                            CultureInfo.InvariantCulture);
+            string date = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss.FFFFF", CultureInfo.InvariantCulture);
 
-            string text = $"{date}: {message}";
+            string text = $"{date}: {caller} {message}";
 
             // полная перезапись файла 
             using (StreamWriter writer = new StreamWriter(_path, true, Encoding.UTF8))
@@ -62,8 +50,60 @@ namespace dRz.Test.OpenDwg
             }
         }
 
+        public void Log(string message, int rowNumber, [CallerMemberName] string caller = null)
+        {
+
+            List<string> lst = new List<string>();
+            using (StreamReader sreader = new StreamReader(_path))
+            {
+                while (!sreader.EndOfStream)
+                {
+                    lst.Add(sreader.ReadLine());
+                }
+
+            }
+
+            string date = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss.FFFFF", CultureInfo.InvariantCulture);
+
+            string text = $"{date}: {caller} {message}";
+
+            //lst[0] = text;
+
+            lst.Insert(rowNumber, text);
+
+            using (StreamWriter writer = new StreamWriter(_path, false, Encoding.UTF8))
+            {
+                foreach (string line in lst)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+
+        void LogDelToDate()
+        {
+            var files = new DirectoryInfo(Path.GetDirectoryName(_path)).GetFiles("*.LOG");
+            foreach (var file in files)
+            {
+                if (DateTime.UtcNow - file.LastWriteTimeUtc > TimeSpan.FromDays(_dateSpan))
+                {
+                    try
+                    {
+                    File.Delete(file.FullName);
+                    Console.WriteLine($"{file.LastWriteTimeUtc/*CreationTimeUtc*/} deleted:\t{file.Name}");
+
+                    }
+                    catch { }
+                }
+            }
+
+        }
+
+
+
         string _appName = "";
 
+        int _dateSpan = 6;
         private string _path => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), _appName);
     }
 }
