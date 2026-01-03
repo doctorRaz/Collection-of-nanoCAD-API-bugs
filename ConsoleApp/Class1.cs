2be758381d2aef44fe101804776a3eb2285e5b6e
doctorRAZ <doctorRAZ@yandex.ru>
2026-01-03 21:08:17 +0300

    nlog пишет в разные файлы, но коряво((

---
+++ ConsoleApp/Class1.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using NLog.Config;
+using System;
+
+namespace ConsoleApp
+{
+
+    public class Class1
+    {
+
+        private static readonly Logger log = NLog.LogManager.GetCurrentClassLogger();
+
+        internal void test1()
+        {
+         Program.ConfigureNLog();
+
+            log.Trace($"Trace");
+            log.Info($"Info");
+            log.Debug($"Debug");
+            log.Warn($"Warn");
+            log.Error($"Error");
+            log.Fatal($"Fatal");
+
+        }
+
+
+
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,62 +1,97 @@
 ﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace ConsoleApp
 {
     internal class Program
     {
 
-        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private static readonly NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
 
+
+        public static void ConfigureNLog([CallerMemberName] string caller = null)
+        {
+            string date = DateTime.Now.ToString("yyyyMMdd-HH_mm_ss", CultureInfo.InvariantCulture);
+
+            var config = new LoggingConfiguration();
+
+            // Target для отдельного класса
+            var fileTarget = new FileTarget
+            {
+                Name = $"{caller}",
+                FileName = $"{date}_{caller}.log",
+                //Layout = "${longdate} ${level:uppercase=true} ${message}"
+            };
+
+            var fileTargetErr = new FileTarget
+            {
+                Name = $"{caller}",
+                FileName = $"{date}_{caller}_Err.log",
+                //Layout = "${longdate} ${level:uppercase=true} ${message}"
+            };
+
+            config.AddRule(LogLevel.Trace, LogLevel.Warn, fileTarget);
+             config.AddRule(LogLevel.Error, LogLevel.Fatal, fileTargetErr);
+
+            //config.AddTarget(fileTarget);
+
+            //// Правило только для конкретного класса
+            //config.AddRuleForOneLevel(LogLevel.Trace, fileTarget, "MyNamespace.MyClass");
+
+            LogManager.Configuration = config;
+        }
 
         static void Main(string[] args)
         {
+
+            ConfigureNLog();
+
             //https://nlog-project.org/
+            /*
 
-            string date = DateTime.Now.ToString("yyyyMMdd", CultureInfo.InvariantCulture);
-
-            var config = new NLog.Config.LoggingConfiguration();
+            LoggingConfiguration config = new NLog.Config.LoggingConfiguration();
 
             // Targets where to log to: File and Console
-            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = $"{date}_nLog.log" };
-            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+            FileTarget logfile = new FileTarget("logfile") { FileName = $"{date}_nLog.log" };
+            var logfileErr = new FileTarget("logfile") { FileName = $"{date}_Err_nLog.log" };
+            ColoredConsoleTarget logconsole = new NLog.Targets.ColoredConsoleTarget("logconsole");
+            
 
             // Rules for mapping loggers to targets            
             //config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
             config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile);
+            config.AddRule(LogLevel.Error, LogLevel.Fatal, logfileErr);
+
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logconsole);
 
             // Apply config           
-            NLog.LogManager.Configuration = config;
+            LogManager.Configuration = config;
+            */
+
+            log.Info("This is a message from {User}", "Mickey Donovan");
+
+            var msg = new LogEventInfo(LogLevel.Info, "", "This is a message");
+            msg.Properties.Add("User", "Ray Donovan");
+            log.Info(msg);
+
+            log.Info(string.Format("This is a message from {0}", "Mickey Donovan"));
 
 
-            Logger.Info($"----------------------");
+            log.Trace($"Trace");
+            log.Debug($"Debug");
+            log.Info($"Info");
+            log.Warn($"Warn");
+            log.Error($"Error");
+            log.Fatal($"Fatal");
 
+            log.Error(new Exception(), "This is an error message");
 
-            Logger.Info($"Info");
-            Logger.Trace($"Trace");
-            Logger.Debug($"Debug");
-            Logger.Warn($"Warn");
-            Logger.Fatal($"Fatal");
-            Logger.Error($"Error");
+            Class1 class1 = new Class1();
+            class1.test1();
 
-
-
-            return;
-            var prop = new Props();
-
-            prop.TestProps();
-
-
-
-
-            #region Logger
-
-
-            var runnner = new LoggerRun();
-
-            runnner.Run();
-
-            #endregion
 
         }
     }
