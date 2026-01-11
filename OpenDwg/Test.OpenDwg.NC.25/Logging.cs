44e2edd8defc8d68a20c806bba8fe66d8500ef6b
doctorRAZ <doctorRAZ@yandex.ru>
2026-01-11 19:18:19 +0300

    подготовка вывода Nlog в консоль КАД

---
+++ OpenDwg/Test.OpenDwg.NC.25/Logging.cs
@@ -0,0 +1,27 @@
+﻿using HostMgd.ApplicationServices;
+using HostMgd.EditorInput;
+using NLog;
+using NLog.Targets;
+
+namespace MyPlugin.Logging
+{
+
+    /// <summary>
+    /// Кастомный Target
+    /// </summary>
+    /// <seealso cref="NLog.Targets.TargetWithLayout" />
+    [Target("AutoCadCommandLine")]
+    public class AutoCadCommandLineTarget : TargetWithLayout
+    {
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null) return;
+
+            Editor ed = doc.Editor;
+            string message = Layout.Render(logEvent);
+
+            ed.WriteMessage("\n" + message);
+        }
+    }
+}
