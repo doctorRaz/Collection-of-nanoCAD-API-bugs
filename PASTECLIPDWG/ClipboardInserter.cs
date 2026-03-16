cf1afff88b06f1696585b46c5bfc1060883de8a1
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2026-03-16 15:52:05 +0300

    вставка из буфера

---
+++ PASTECLIPDWG/ClipboardInserter.cs
@@ -0,0 +1,102 @@
+﻿using HostMgd.ApplicationServices;
+using HostMgd.EditorInput;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Teigha.Geometry;
+using Teigha.Runtime;
+
+
+//(command "-INSERT" "c:\\temp\\a6df.dwg" "0,0,0" "1" "1" "0")
+
+namespace dRz.nanoCADbugs
+{
+    public partial class ClipboardInserter
+    {
+        // Команда для NanoCAD
+        [CommandMethod("NC_PASTECLIPDWG")]
+        public void PasteClipboardDwg()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+
+            try
+            {
+                string dwg = FindClipboardDwg();
+
+                if (dwg == null)
+                {
+                    ed.WriteMessage("\nDWG из буфера не найден.");
+                    return;
+                }
+
+                Point3d pt = AskInsertPoint(ed);
+
+                ExecuteInsert(doc, dwg, pt);
+
+                ExecuteExplodeLast(doc);
+            }
+            catch (System.Exception ex)
+            {
+                ed.WriteMessage($"\nОшибка: {ex.Message}");
+            }
+        }
+
+        private string FindClipboardDwg()
+        {
+            string temp = Path.GetTempPath();
+
+            FileInfo? file = new DirectoryInfo(temp)
+                .GetFiles("*.dwg")
+                .OrderByDescending(f => f.LastWriteTime)
+                .FirstOrDefault();
+
+            return file?.FullName;
+        }
+
+        private Point3d AskInsertPoint(Editor ed)
+        {
+            PromptPointResult p = ed.GetPoint("\nУкажите точку вставки: ");
+
+            if (p.Status != PromptStatus.OK)
+            {
+                throw new OperationCanceledException();
+            }
+
+            return p.Value;
+        }
+
+        private void ExecuteInsert(Document doc, string dwg, Point3d pt)
+        {
+            string p = FormatPoint(pt);
+
+            dwg = dwg.Replace("\\", "\\\\");
+
+            string cmd =
+                "-INSERT\n" +
+                $"{dwg}\n" +
+                $"{p}\n" +
+                "1\n" +
+                "1\n" +
+                "0\n";
+
+            doc.SendStringToExecute(cmd, true, false, false);
+
+        }
+
+        private void ExecuteExplodeLast(Document doc)
+        {
+            doc.SendStringToExecute("EXPLODE L\n", true, false, false);
+        }
+
+        private string FormatPoint(Point3d pt)
+        {
+            return string.Format(
+                CultureInfo.InvariantCulture,
+                "{0},{1},{2}",
+                pt.X, pt.Y, pt.Z);
+        }
+
+    }
+}
---
+++ PASTECLIPDWG/README.md
@@ -0,0 +1,23 @@
+# NC_PASTECLIPDWG
+
+[Бага обсуждается тут](https://forum.nanocad.ru/t/vstavka-bloka-iz-bufera-obmena/1752)
+[Тут пихают под нее костылики](https://forum.nanocad.ru/t/komandnyj-rezhim-komandy-pblok/1715/12)
+
+![уот так](https://forum.nanocad.ru/uploads/default/original/2X/3/39d46055f0465950c43eeb45b8da5b8c7c326b24.png)
+
+>  Явно не хватает варианта “Использовать существующее определение”. 
+
+ 
+ ---
+
+попрошу тапками не кидать))
+
+копируем фрагмент чертежа как обычно\
+по <kbd>Ctrl</kbd> + <kbd>C</kbd> или <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>C</kbd>
+
+вставляем "из буфера" командой `NC_PASTECLIPDWG`\
+используется существующее определение блоков
+
+---
+
+PS код не мой, жопите писал я немного поправил)))
